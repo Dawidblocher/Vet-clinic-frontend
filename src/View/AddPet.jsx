7954c8423776c3be78f21efcc9864d972d4802b2
Dawid Blocher <blocher1997@gmail.com>
2021-08-24 23:26:24 +0200

    added Formik and created a form to add a new pet

--- src/View/AddPet.jsx
+++ src/View/AddPet.jsx
@@ -3,53 +3,81 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 import 'date-fns';
-import Grid from '@material-ui/core/Grid';
+import {Grid, Paper, Button} from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
-
+import { useFormik } from 'formik';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       '& .MuiTextField-root': { 
         margin: theme.spacing(1),
-        width: '25ch',
+        width: 'calc(100% - 15px)',
       },
+      display: 'flex',
+      flexDirection: 'column'
     },
-  }));
+    gridCenter: {
+      margin: '0 auto'
+    },
+    paperColumn: {
+      display: 'flex',
+      flexDirection: 'column'
+    }
+}));
 
 const AddPet = () => {
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18'));
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   }; 
-    return (
+  
+  const formik = useFormik({
+    initialValues: {
+      name: 'test',
+      specs: '',
+      breed: '',
+      birthdate: selectedDate
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-      <TextField id="outlined-search" label="Imię zwierzaka" type="text" variant="outlined" />
-        <TextField id="outlined-search" label="Gatunek" type="text" variant="outlined" />
-        <TextField id="outlined-search" label="Rasa" type="text" variant="outlined" />
-        <MuiPickersUtilsProvider utils={DateFnsUtils}><KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Data urodzenia"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        /></MuiPickersUtilsProvider>
-      </div>
-    </form>
-)
-        }
+  return (
+    <Grid className={classes.gridCenter} item xs={12} md={6} lg={6} >
+      <Paper className={classes.paperColumn}>         
+        <form className={classes.root} onSubmit={formik.handleSubmit}>
+          <TextField id="name" name="name" label="Imię zwierzaka" type="text" variant="outlined" value={formik.values.name} onChange={formik.handleChange}/>
+            <TextField id="specs" name="specs" label="Gatunek" type="text" variant="outlined" onChange={formik.handleChange}/>
+            <TextField id="breed" name="breed" label="Rasa" type="text" variant="outlined" onChange={formik.handleChange} />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}><KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="birthdate"
+              name="birthdate"
+              label="Data urodzenia"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            /></MuiPickersUtilsProvider> 
+          <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+        </form>
+      </Paper>
+    </Grid>
+    
+  )
+}
 
 export default AddPet
