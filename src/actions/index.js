40c8d65a8cf8ca2811fd70613324f6ee823b839f
Dawid Blocher <blocher1997@gmail.com>
2021-09-15 21:03:41 +0200

    refactor code and add pet with API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,67 @@
 import axios from 'axios'
 
-export const ADD_PET = 'ADD_PET'
+export const ADD_PET_REQUEST = 'ADD_PET_REQUEST'
+export const ADD_PET_SUCCESS = 'ADD_PET_SUCCESS'
+export const ADD_PET_FAILURE = 'ADD_PET_FAILURE'
+
 export const FETCH_REQUEST = 'FETCH_REQUEST'
 export const FETCH_SUCCESS = 'FETCH_SUCCESS'
 export const FETCH_FAILURE = 'FETCH_FAILURE'
 
-export const addPet = ( pet ) =>({
-    type: ADD_PET,
-    payload: {
-        id: pet.id,
-        name: pet.name,
-        specs: pet.specs,
-        breed: pet.breed,
-        birthdate: pet.birthdate,
-        ownerId: pet.ownerId
-    }
-})
+export const FETCH_VISIT_REQUEST = 'FETCH_VISIT_REQUEST'
+export const FETCH_VISIT_SUCCESS = 'FETCH_VISIT_SUCCESS'
+export const FETCH_VISIT_FAILURE = 'FETCH_VISIT_FAILURE'
+
+export const addPet = ( pet ) => (dispatch) => {
+    dispatch({type: ADD_PET_REQUEST })
+    
+    axios({
+        method: 'post',
+        url: 'http://localhost:8080/pet',
+        data: {
+            name: 'test',
+            specs: 'test',
+            breed: 'test',
+            owner: {
+                id: 1
+            },
+            birthDate: "10-10-2020"
+        }
+      });
+    return axios.post(`http://localhost:8080/pet`,{
+        name: 'test',
+        specs: 'test',
+        breed: 'test',
+        owner: {
+            id: 1
+        },
+        birthDate: "10-10-2020"
+    }).then(payload => {
+        console.log(payload)
+        // dispatch({type: ADD_PET_SUCCESS, payload})
+    }).catch(error => {
+        // Error
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            // console.log(error.response.data);
+            // console.log(error.response.status);
+            // console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the 
+            // browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+        dispatch({type: ADD_PET_FAILURE })
+    })
+    
+}
 
 export const getDoctors = (specs = null) => (dispatch, getState) => {
     dispatch({type: FETCH_REQUEST})
@@ -23,6 +69,7 @@ export const getDoctors = (specs = null) => (dispatch, getState) => {
     return axios
     .get(`http://localhost:8080/doctor`)
     .then(({data}) => {
+
         dispatch({type: FETCH_SUCCESS, data})
     })
     .catch(err => {
@@ -30,4 +77,18 @@ export const getDoctors = (specs = null) => (dispatch, getState) => {
         dispatch({type: FETCH_FAILURE})
     })
 
+}
+
+export const getVisits = () => (dispatch) => {
+    dispatch({type: FETCH_VISIT_REQUEST})
+
+    return axios
+    .get(`http://localhost:8080/visit`)
+    .then(({data}) => {
+        dispatch({type: FETCH_VISIT_SUCCESS, data})
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch({type: FETCH_VISIT_FAILURE})
+    })
 }
